ee6c02ee46919751566570f9936f17bbbeccf375
Razvan Gheorghisan <gheorghisanr@gmail.com>
2026-06-27 16:18:32 +0300

    add enum for flags

--- src/core/cpu.h
+++ src/core/cpu.h
@@ -3,6 +3,19 @@
 
 #include<stdint.h>
 
+typedef struct Bus Bus;
+
+typedef enum FLAGS {
+    C = (1 << 0), // carry bit
+    Z = (1 << 1), // zero
+    I = (1 << 2), // interrupt disable
+    D = (1 << 3), // decimal
+    B = (1 << 4), // break
+    U = (1 << 5), // unused
+    V = (1 << 6), // overflow
+    N = (1 << 7), // negative
+};
+
 typedef struct CPU {
     // registers
     uint8_t a;
@@ -19,7 +32,7 @@ typedef struct CPU {
     uint8_t addr_add;
     uint8_t addr_rel;
 
-
+    Bus *bus;
 } CPU;
 
 #endif
