9c4a57b59b43c6b85b814b7634a7086b0cd67d22
Razvan Gheorghisan <gheorghisanr@gmail.com>
2026-06-28 20:35:12 +0300

    mostly finished bus.c

--- src/core/bus.c
+++ src/core/bus.c
@@ -7,10 +7,13 @@ void bus_init(Bus *bus)
 
 uint8_t bus_read(Bus *bus, uint16_t addr)
 {
-
+    if (addr >= 0x0000 && addr <= 0xFFFF)
+        return bus->ram[addr];
+    return 0x00;
 }
 
 void bus_write(Bus *bus, uint16_t addr, uint8_t data)
 {
-    
+    if (addr >= 0x0000 && addr <= 0xFFFF)
+        bus->ram[addr] = data;
 }
