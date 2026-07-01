3dd7648336976c6f009b2bfe50b5a9faca906583
Razvan Gheorghisan <gheorghisanr@gmail.com>
2026-07-01 15:10:04 +0300

    implement cpu_clock function

--- src/core/cpu.c
+++ src/core/cpu.c
@@ -1,5 +1,4 @@
 #include "bus.h"
-#include "cpu.h"
 #include "cpu_internal.h"
 
 void cpu_connect_bus(CPU *cpu, Bus *bus)
@@ -7,11 +6,35 @@ void cpu_connect_bus(CPU *cpu, Bus *bus)
     cpu->bus = bus;
 }
 
+uint8_t cpu_get_flag(CPU *cpu, Flags f)
+{
+    return (cpu->status & f) ? 1 : 0;
+}
+
+void cpu_set_flag(CPU *cpu, Flags f, uint8_t val)
+{
+    if (val)
+        cpu->status |= f;
+    else
+        cpu->status &= ~f;
+}
+
 void cpu_clock(CPU *cpu)
 {
     if (cpu->cycles == 0) {
+        cpu->opcode = bus_read(cpu->bus, cpu->pc);
+        cpu->pc++;
+        cpu_set_flag(cpu, FLAG_U, 1);
 
+        instruction instr = cpu_lookup[cpu->opcode];
+        uint8_t addr_ret = instr.addrmode(cpu);
+        uint8_t op_ret = instr.operate(cpu);
+
+        cpu->cycles += (addr_ret & op_ret);
+
+        cpu_set_flag(cpu, FLAG_U, 1);
     }
+    cpu->cycles--;
 }
 
 void cpu_reset(CPU *cpu)
@@ -49,20 +72,7 @@ void cpu_nmi(CPU *cpu)
 
 }
 
-int cpu_complete(CPU *cpu)
+int cpu_complete(const CPU *cpu)
 {
-
-}
-
-uint8_t cpu_get_flag(CPU *cpu, Flags f)
-{
-    return (cpu->status & f) ? 1 : 0;
-}
-
-void cpu_set_flag(CPU *cpu, Flags f, uint8_t val)
-{
-    if (val)
-        cpu->status |= f;
-    else
-        cpu->status &= ~f;
+    return cpu->cycles == 0;
 }
