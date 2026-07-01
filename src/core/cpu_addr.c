2b7ef7ee347d8efffa6e70d28171be04540f6857
Razvan Gheorghisan <gheorghisanr@gmail.com>
2026-07-01 12:06:35 +0300

    add empty functions for all address modes

---
+++ src/core/cpu_addr.c
@@ -0,0 +1,76 @@
+#include "cpu_internal.h"
+#include "bus.h"
+
+// addressing modes
+
+// implicit
+uint8_t IMP(CPU *cpu)
+{
+
+}
+
+// #v (immediate)
+uint8_t IMM(CPU *cpu)
+{
+
+}
+
+// d (zero page)
+uint8_t ZP0(CPU *cpu)
+{
+
+}
+
+// d,x (zero page offset by x)
+uint8_t ZPX(CPU *cpu)
+{
+
+}
+
+// d,y (zero page offset by y)
+uint8_t ZPY(CPU *cpu)
+{
+
+}
+
+// a (absolute)
+uint8_t ABS(CPU *cpu)
+{
+
+}
+
+// a,x (absolute offset by x)
+uint8_t ABX(CPU *cpu)
+{
+
+}
+
+// a,y (absolute offset by y)
+uint8_t ABY(CPU *cpu)
+{
+
+}
+
+// label (relative)
+uint8_t REL(CPU *cpu)
+{
+
+}
+
+// (a) (indirect)
+uint8_t IND(CPU *cpu)
+{
+
+}
+
+// (d,x) (indirect x)
+uint8_t IZX(CPU *cpu)
+{
+
+}
+
+// (d),y (indirect y)
+uint8_t IZY(CPU *cpu)
+{
+
+}
