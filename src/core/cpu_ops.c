359ab0fb6f0cb1f075eac562b8a41e75d1ec149c
Razvan Gheorghisan <gheorghisanr@gmail.com>
2026-07-12 22:56:10 +0300

    implement and fix several instructions

--- src/core/cpu_ops.c
+++ src/core/cpu_ops.c
@@ -14,7 +14,7 @@ uint8_t ADC(CPU *cpu)
 
     cpu->a = result & 0x00FF;
 
-    return 0;
+    return 1;
 }
 
 uint8_t AND(CPU *cpu)
@@ -26,11 +26,17 @@ uint8_t AND(CPU *cpu)
     cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
     cpu_set_flag(cpu, FLAG_N, cpu->a & 0x80);
 
-    return 0;
+    return 1;
 }
 
 uint8_t ASL(CPU *cpu)
 {
+    cpu_set_flag(cpu, FLAG_C, cpu->a & 0x80);
+    cpu->a <<= 1;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->a & 0x80);
+
     return 0;
 }
 
@@ -87,7 +93,7 @@ uint8_t BIT(CPU *cpu)
     cpu_set_flag(cpu, FLAG_V, cpu->fetched & 0x40);
     cpu_set_flag(cpu, FLAG_N, cpu->fetched & 0x80);
 
-    return 0;
+    return 1;
 }
 
 uint8_t BMI(CPU *cpu)
@@ -202,37 +208,37 @@ uint8_t CLV(CPU *cpu)
 uint8_t CMP(CPU *cpu)
 {
     cpu_fetch(cpu);
-    uint8_t result = cpu->a - cpu->fetched;
+    uint16_t result = (uint16_t)cpu->a - (uint16_t)cpu->fetched;
 
-    cpu_set_flag(cpu, FLAG_C, result >= 0);
-    cpu_set_flag(cpu, FLAG_Z, result == 0);
+    cpu_set_flag(cpu, FLAG_C, cpu->a >= cpu->fetched);
+    cpu_set_flag(cpu, FLAG_Z, (result & 0x00FF) == 0);
     cpu_set_flag(cpu, FLAG_N, result & 0x80);
 
-    return 0;
+    return 1;
 }
 
 uint8_t CPX(CPU *cpu)
 {
     cpu_fetch(cpu);
-    uint8_t result = cpu->x - cpu->fetched;
+    uint16_t result = (uint16_t)cpu->x - (uint16_t)cpu->fetched;
 
-    cpu_set_flag(cpu, FLAG_C, result >= 0);
-    cpu_set_flag(cpu, FLAG_Z, result == 0);
+    cpu_set_flag(cpu, FLAG_C, cpu->x >= cpu->fetched);
+    cpu_set_flag(cpu, FLAG_Z, (result & 0x00FF) == 0);
     cpu_set_flag(cpu, FLAG_N, result & 0x80);
 
-    return 0;
+    return 1;
 }
 
 uint8_t CPY(CPU *cpu)
 {
     cpu_fetch(cpu);
-    uint8_t result = cpu->y - cpu->fetched;
+    uint16_t result = (uint16_t)cpu->y - (uint16_t)cpu->fetched;
 
-    cpu_set_flag(cpu, FLAG_C, result >= 0);
-    cpu_set_flag(cpu, FLAG_Z, result == 0);
+    cpu_set_flag(cpu, FLAG_C, cpu->y >= cpu->fetched);
+    cpu_set_flag(cpu, FLAG_Z, (result & 0x00FF) == 0);
     cpu_set_flag(cpu, FLAG_N, result & 0x80);
 
-    return 0;
+    return 1;
 }
 
 uint8_t DEC(CPU *cpu)
@@ -242,11 +248,21 @@ uint8_t DEC(CPU *cpu)
 
 uint8_t DEX(CPU *cpu)
 {
+    cpu->x--;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->x == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->x & 0x80);
+
     return 0;
 }
 
 uint8_t DEY(CPU *cpu)
 {
+    cpu->y--;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->y == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->y & 0x80);
+
     return 0;
 }
 
@@ -258,21 +274,34 @@ uint8_t EOR(CPU *cpu)
     cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
     cpu_set_flag(cpu, FLAG_N, cpu->a & 0x80);
 
-    return 0;
+    return 1;
 }
 
 uint8_t INC(CPU *cpu)
 {
-    return 0;
+    cpu_fetch(cpu);
+    //bus_write(cpu->bus, cpu->addr_abs, cpu->fetched + 1);
+
+    return 1;
 }
 
 uint8_t INX(CPU *cpu)
 {
+    cpu->x++;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->x == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->x & 0x80);
+
     return 0;
 }
 
 uint8_t INY(CPU *cpu)
 {
+    cpu->y++;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->y == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->y & 0x80);
+
     return 0;
 }
 
@@ -335,6 +364,12 @@ uint8_t LDY(CPU *cpu)
 
 uint8_t LSR(CPU *cpu)
 {
+    cpu_set_flag(cpu, FLAG_C, cpu->a & 0x01);
+    cpu->a >>= 1;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
+    cpu_set_flag(cpu, FLAG_N, 0);
+
     return 0;
 }
 
@@ -352,7 +387,7 @@ uint8_t ORA(CPU *cpu)
     cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
     cpu_set_flag(cpu, FLAG_N, cpu->a & 0x80);
 
-    return 0;
+    return 1;
 }
 
 uint8_t PHA(CPU *cpu)
@@ -397,16 +432,50 @@ uint8_t PLP(CPU *cpu)
 
 uint8_t ROL(CPU *cpu)
 {
+    uint8_t old_C = cpu_get_flag(cpu, FLAG_C);
+    cpu_set_flag(cpu, FLAG_C, cpu->a & 0x80);
+    cpu->a <<= 1;
+    cpu->a |= old_C;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->a & 0x80);
+
     return 0;
 }
 
 uint8_t ROR(CPU *cpu)
 {
+    uint8_t old_C = cpu_get_flag(cpu, FLAG_C);
+    cpu_set_flag(cpu, FLAG_C, cpu->a & 0x01);
+    cpu->a >>= 1;
+    cpu->a |= (old_C << 7);
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->a & 0x80);
+
     return 0;
 }
 
 uint8_t RTI(CPU *cpu)
 {
+    cpu->stkp++;
+    uint8_t flags = bus_read(cpu->bus, 0x0100 + cpu->stkp);
+    cpu->stkp++;
+    uint16_t lo = bus_read(cpu->bus, 0x0100 + cpu->stkp);
+    cpu->stkp++;
+    uint16_t hi = bus_read(cpu->bus, 0x0100 + cpu->stkp);
+
+    cpu->pc = (hi << 8) | lo;
+
+    cpu_set_flag(cpu, FLAG_N, flags & FLAG_N);
+    cpu_set_flag(cpu, FLAG_V, flags & FLAG_V);
+    cpu_set_flag(cpu, FLAG_B, 0);
+    cpu_set_flag(cpu, FLAG_U, 1);
+    cpu_set_flag(cpu, FLAG_D, flags & FLAG_D);
+    cpu_set_flag(cpu, FLAG_I, flags & FLAG_I);
+    cpu_set_flag(cpu, FLAG_Z, flags & FLAG_Z);
+    cpu_set_flag(cpu, FLAG_C, flags & FLAG_C);
+
     return 0;
 }
 
@@ -424,7 +493,17 @@ uint8_t RTS(CPU *cpu)
 
 uint8_t SBC(CPU *cpu)
 {
-    return 0;
+    cpu_fetch(cpu);
+    uint16_t result = (uint16_t)cpu->a + ((uint16_t)cpu->fetched ^ 0x00FF) + (cpu->status & FLAG_C);
+
+    cpu_set_flag(cpu, FLAG_C, result > 0xFF);
+    cpu_set_flag(cpu, FLAG_Z, (result & 0x00FF) == 0);
+    cpu_set_flag(cpu, FLAG_V, (result ^ cpu->a) & (result ^ (uint16_t)cpu->fetched ^ 0x00FF) & 0x80);
+    cpu_set_flag(cpu, FLAG_N, result & 0x80);
+
+    cpu->a = result & 0x00FF;
+
+    return 1;
 }
 
 uint8_t SEC(CPU *cpu)
@@ -468,31 +547,58 @@ uint8_t STY(CPU *cpu)
 
 uint8_t TAX(CPU *cpu)
 {
+    cpu->x = cpu->a;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->x == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->x & 0x80);
+
     return 0;
 }
 
 uint8_t TAY(CPU *cpu)
 {
+    cpu->y = cpu->a;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->y == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->y & 0x80);
+
     return 0;
 }
 
 uint8_t TSX(CPU *cpu)
 {
+    cpu->x = cpu->stkp;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->x == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->x & 0x80);
+
     return 0;
 }
 
 uint8_t TXA(CPU *cpu)
 {
+    cpu->a = cpu->x;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->a & 0x80);
+
     return 0;
 }
 
 uint8_t TXS(CPU *cpu)
 {
+    cpu->stkp = cpu->x;
+
     return 0;
 }
 
 uint8_t TYA(CPU *cpu)
 {
+    cpu->a = cpu->y;
+
+    cpu_set_flag(cpu, FLAG_Z, cpu->a == 0);
+    cpu_set_flag(cpu, FLAG_N, cpu->a & 0x80);
+
     return 0;
 }
 
