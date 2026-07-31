f97ae4d4e823dddcbafbc23a0343c4e29dfc0738
Razvan Gheorghisan <gheorghisanr@gmail.com>
2026-07-31 17:17:36 +0300

    add pointers to per mapper functions

--- src/core/mappers/mapper.h
+++ src/core/mappers/mapper.h
@@ -7,6 +7,10 @@ typedef struct Mapper {
     uint8_t prg_banks;
     uint8_t chr_banks;
     
+    int (*cpu_map_read)(Mapper *m, uint16_t addr, uint32_t *mapped);
+    int (*cpu_map_write)(Mapper *m, uint16_t addr, uint32_t *mapped);
+    int (*ppu_map_read)(Mapper *m, uint16_t addr, uint32_t *mapped);
+    int (*ppu_map_write)(Mapper *m, uint16_t addr, uint32_t *mapped);
     
 } Mapper;
 
