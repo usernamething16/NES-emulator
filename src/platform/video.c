0f949e04ec8cddc29117b7d83dceb4a5cd6edc64
Razvan Gheorghisan <gheorghisanr@gmail.com>
2026-08-09 23:38:08 +0300

    skeleton files for video

---
+++ src/platform/video.c
@@ -0,0 +1,8 @@
+#include "video.h"
+#include <SDL3/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Video {
+
+};
---
+++ src/platform/video.h
@@ -0,0 +1,8 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include <stdint.h>
+
+typedef struct Video Video;
+
+#endif
