7d16a2ff253c8880f7e62dea38723842b2dee4f3
Razvan Gheorghisan <gheorghisanr@gmail.com>
2026-08-15 22:58:26 +0300

    incomplete, but have to save

--- src/platform/video.c
+++ src/platform/video.c
@@ -10,3 +10,20 @@ struct Video {
     int tex_w;
     int tex_h;
 };
+
+Video *Video_Create(const char *title, int tex_w, int tex_h, int win_w, int win_h)
+{
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "Sdl_Init failed");
+        return NULL;
+    }
+
+    Video *v = calloc(1, sizeof v);
+    if (!v) {
+        SDL_Quit();
+        return NULL;
+    }
+
+    v->tex_w = tex_w;
+    v->tex_h = tex_h;
+}
